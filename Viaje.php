67ce59ba6b042c4989cb1a89f95ee314b69c6e40
Hector <hectorpohle@gmail.com>
2022-04-01 18:44:40 -0300

    implementacion de funciones en el test

--- Viaje.php
+++ Viaje.php
@@ -1,9 +1,12 @@
 <?php
 
 /* 
-La empresa de Transporte de Pasajeros “Viaje Feliz” quiere registrar la información referente a sus viajes. De cada viaje se precisa almacenar el código del mismo, destino, cantidad máxima de pasajeros y los pasajeros del viaje.
+La empresa de Transporte de Pasajeros “Viaje Feliz” quiere registrar la información referente a sus viajes. 
+De cada viaje se precisa almacenar el código del mismo, destino, cantidad máxima de pasajeros y los pasajeros del viaje.
 
-Realice la implementación de la clase Viaje e implemente los métodos necesarios para modificar los atributos de dicha clase (incluso los datos de los pasajeros). Utilice un array que almacene la información correspondiente a los pasajeros. Cada pasajero es un array asociativo con las claves “nombre”, “apellido” y “numero de documento”.
+Realice la implementación de la clase Viaje e implemente los métodos necesarios para modificar los atributos de dicha clase (incluso los datos de los pasajeros). 
+Utilice un array que almacene la información correspondiente a los pasajeros. 
+Cada pasajero es un array asociativo con las claves “nombre”, “apellido” y “numero de documento”.
 
 Implementar un script testViaje.php que cree una instancia de la clase Viaje y presente un menú que permita cargar la información del viaje, modificar y ver sus datos. 
 */
@@ -11,10 +14,10 @@ Implementar un script testViaje.php que cree una instancia de la clase Viaje y p
 class Viaje{
     
 //ATRIBUTOS
-    public $codigo;
-    public $destino;
-    public $cantidadMax;
-    public $pasajerosDelViaje;
+    private $codigo;
+    private $destino;
+    private $cantidadMax;
+    private $pasajerosDelViaje = [];
 
 //CONSTRUCTOR
     public function __construct($codigo, $destino, $cantidadMax, $pasajerosDelViaje)
@@ -69,16 +72,31 @@ class Viaje{
 //__toString
     public function __toString()
     {
-        $v_codigo = $this->getCodigo();
-        $v_destino = $this->getDestino();
-        $v_cantMax = $this->getCantidadMax();
-        $v_pasajeros = $this->getPasajerosDelViaje();
         return (
-        "Código del viaje: " . $v_codigo . "\n" .
-        "Destino del viaje: " . $v_destino . "\n" .
-        "Cantidad máxima de pasajeros: " . $v_cantMax . "\n" . 
-        "Pasajeros: " . $v_pasajeros . "\n"
+        "----- VIAJE -----\n".
+        "Código del viaje: " . $this->getCodigo() . "\n" .
+        "Destino del viaje: " . $this->getDestino() . "\n" .
+        "Cantidad máxima de pasajeros: " . $this->getCantidadMax() . "\n" . 
+        "Pasajeros: " . $this->getPasajerosDelViaje() . "\n"
         );
+    }
+
+    //OBTIENE EL NOMBRE DE UN PASAJERO SEGUN UN INDICE
+    public function obtenerNombrePasajero($indice){
+        $i_pasajero_nombre = ($this->getPasajerosDelViaje())[$indice -1]["nombre"];
+        return $i_pasajero_nombre;
+    }
+
+    //OBTIENE EL APELLIDO DE UN PASAJERO SEGUN UN INDICE
+    public function obtenerApellidoPasajero($indice){
+        $i_pasajero_apellido = ($this->getPasajerosDelViaje())[$indice -1]["apellido"];
+        return $i_pasajero_apellido;
+    }
+
+    //OBTIENE EL DNI DE UN PASAJERO SEGUN UN INDICE
+    public function obtenerDNIPasajero($indice){
+        $i_pasajero_dni = ($this->getPasajerosDelViaje())[$indice -1]["dni"];
+        return $i_pasajero_dni;
     }
 }
 ?>
--- test_Viaje.php
+++ test_Viaje.php
@@ -2,14 +2,107 @@
 
 include "Viaje.php";
 
+//Array de muestra de los pasajeros del viaje.
 $v_pasajeros = array(
-    0 => ["nombre" => "Juan", "apellido" => "Perez", "dni" => 11457338],
+    0 => ["nombre" => "Juan", "apellido" => "Perez", "dni" => 21457338],
     1 => ["nombre" => "Oscar", "apellido" => "García", "dni" => 31457558],
     2 => ["nombre" => "Rosa", "apellido" => "Gonzalez", "dni" => 21457838],
     3 => ["nombre" => "María", "apellido" => "Diaz", "dni" => 13457638],
-    4 => ["nombre" => "Alejandro", "apellido" => "Mendez", "dni" => 12457438],
+    4 => ["nombre" => "Alejandro", "apellido" => "Mendez", "dni" => 42457438],
 );
 
+//Creacion de un nuevo objeto Viaje de muestra
 $objViaje = new Viaje(125, "Bariloche", 55, $v_pasajeros);
+
+//FUNCION CREAR NUEVO VIAJE
+function nuevoViaje($arr){
+    echo "Ingrese el código del viaje: \n";
+    $n_codigo = trim(fgets(STDIN));
+    echo "Ingrese el destino: \n";
+    $n_destino = trim(fgets(STDIN));
+    echo "Ingrese cantidad máxima de pasajeros: \n";
+    $n_cantMax = trim(fgets(STDIN));
+    $n_pasajeros = $arr;
+    $n_viaje = new Viaje($n_codigo, $n_destino, $n_cantMax, $n_pasajeros);
+    return $n_viaje;
+};
+
+/**
+ * Recibe un array con todos los pasajeros del viaje y los muestra por pantalla.
+ */
+function mostrarPasajeros($arr){
+    $i = 1;
+    foreach ($arr as $valor) {
+        echo "----- PASAJERO " . $i . " -----\n";
+        $i = $i +1;
+        foreach ($valor as $key => $value) { 
+            echo $key . " : " . $value . "\n";
+        }
+        echo "-----   *****   -----\n";
+    }
+}
+
+/**
+ * Recibe un objeto Viaje y muestra por pantalla todos los datos del viaje
+ */
+function mostrarViaje($viaje){
+    echo "Código de viaje: " . $viaje->getCodigo() ."\n";
+    echo "Destino de viaje: {$viaje->getDestino()}.\n";
+    echo "Cantidad máxima de pasajeros: {$viaje->getCantidadMax()}.\n";
+    echo "Pasajeros: \n";
+    mostrarPasajeros($viaje->getPasajerosDelViaje());
+    //echo "-----   *****   -----\n";
+}
+
+/**
+ * Muestra el muenu al usuario para poder interactuar con los datos.
+ */
+function menu(){
+    echo "[1] Ingresar un viaje. \n";
+    echo "[2] Modificar un viaje. \n";
+    echo "[3] Agregar pasajero.\n";
+    echo "[4] Eliminar un pasajero.\n";
+    echo "[5] Modificar un pasajero.\n";
+    echo "[6] Mostrar un viaje.\n";
+    echo "[7] Salir \n";
+
+    do {
+        echo "Elija una opción ";
+        $opcion = trim(fgets(STDIN));
+    } while ($opcion < 1 || $opcion > 7);
+    return $opcion;
+}
+
+do {
+    $opcion = menu();
+    switch ($opcion) {
+        case 1:
+            echo "----- Ingrese un nuevo viaje -----\n";
+                $viaje = nuevoViaje($v_pasajeros);
+            break;
+        case 2:
+            echo "----- Modificar un viaje -----\n";
+
+            break;
+        case 3:
+            echo "----- Agregar un pasajero -----\n";
+
+            break;
+        case 4:
+            echo "----- Eliminar un pasajero -----\n";
+
+            break;
+        case 5:
+            echo "----- Modificar un pasajero -----\n";
+
+            break;
+        case 6:
+            echo "----- Mostrar un viaje -----\n";
+                mostrarViaje($objViaje);
+            break;
+    }
+} while ($opcion <> 7);
+
+
 
 ?>
